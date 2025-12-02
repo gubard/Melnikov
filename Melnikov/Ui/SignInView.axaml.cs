e7c986559dba0f392190c016d82ef48e6766408f
gubard <maksimovsergey08@gmail.com>
2025-12-02 08:25:47 +0200

    Add Enter key navigation for SignInView form elements

--- Melnikov/Ui/SignInView.axaml.cs
+++ Melnikov/Ui/SignInView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Melnikov.Ui;
 
@@ -7,5 +8,25 @@ public partial class SignInView : UserControl
     public SignInView()
     {
         InitializeComponent();
+    }
+
+    private void LoginOrEmailTextBoxOnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter)
+        {
+            return;
+        }
+
+        PasswordTextBox.Focus();
+    }
+
+    private void PasswordTextBoxOnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter)
+        {
+            return;
+        }
+
+        SignInButton.Command?.Execute(SignInButton.CommandParameter);
     }
 }
