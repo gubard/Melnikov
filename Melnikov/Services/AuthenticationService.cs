4643c836874c9a9c385c5a623c5fe65ae2ba04db
gubard <maksimovsergey08@gmail.com>
2025-12-04 11:03:34 +0200

    Refactor `AuthenticationService` to inherit from `Service`, simplifying code and removing redundant logic.

--- Melnikov/Services/AuthenticationService.cs
+++ Melnikov/Services/AuthenticationService.cs
@@ -1,36 +1,8 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
-using Gaia.Helpers;
+﻿using System.Text.Json;
+using Gaia.Services;
 using Manis.Contract.Models;
 using Manis.Contract.Services;
-using Zeus.Helpers;
 
 namespace Melnikov.Services;
 
-public class AuthenticationService : IAuthenticationService
-{
-    private readonly HttpClient _httpClient;
-    private readonly JsonSerializerOptions _jsonSerializerOptions;
-
-    public AuthenticationService(HttpClient httpClient, JsonSerializerOptions jsonSerializerOptions)
-    {
-        _httpClient = httpClient;
-        _jsonSerializerOptions = jsonSerializerOptions;
-    }
-
-    public async ValueTask<ManisGetResponse> GetAsync(ManisGetRequest request, CancellationToken ct)
-    {
-        using var httpResponse = await _httpClient.PostAsJsonAsync(RouteHelper.Get, request, _jsonSerializerOptions, ct);
-        var response = await httpResponse.Content.ReadFromJsonAsync<ManisGetResponse>(_jsonSerializerOptions, ct);
-
-        return response.ThrowIfNull();
-    }
-
-    public async ValueTask<ManisPostResponse> PostAsync(ManisPostRequest request, CancellationToken ct)
-    {
-        using var httpResponse = await _httpClient.PostAsJsonAsync(RouteHelper.Post, request, _jsonSerializerOptions, ct);
-        var response = await httpResponse.Content.ReadFromJsonAsync<ManisPostResponse>(_jsonSerializerOptions, ct);
-
-        return response.ThrowIfNull();
-    }
-}
+public class AuthenticationService(HttpClient httpClient, JsonSerializerOptions jsonSerializerOptions) : Service<ManisGetRequest, ManisPostRequest, ManisGetResponse, ManisPostResponse>(httpClient, jsonSerializerOptions), IAuthenticationService;
