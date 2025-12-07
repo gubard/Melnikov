7c91b31fe1205f32b1524fd0a1bc6aa51a521cde
gubard <maksimovsergey08@gmail.com>
2025-12-07 20:12:55 +0200

    Add support for "Remember Me" and offline sign-in functionality to `SignInViewModel`. Add `MelnikovSettings` model and integrate settings management. Update `SignInView` with initialization and UI enhancements.

---
+++ Melnikov/Models/MelnikovSettings.cs
@@ -0,0 +1,10 @@
+﻿using Nestor.Db.Models;
+
+namespace Melnikov.Models;
+
+[SourceEntity(nameof(Id))]
+public partial class MelnikovSettings
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; } = string.Empty;
+}
--- Melnikov/Ui/SignInViewModel.cs
+++ Melnikov/Ui/SignInViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Gaia.Helpers;
 using Inanna.Helpers;
 using Inanna.Models;
 using Inanna.Services;
 using Manis.Contract.Models;
+using Melnikov.Models;
 using Melnikov.Services;
 
 namespace Melnikov.Ui;
@@ -12,16 +14,34 @@ public partial class SignInViewModel : ViewModelBase, INonHeader
 {
     [ObservableProperty] private string _loginOrEmail = string.Empty;
     [ObservableProperty] private string _password = string.Empty;
+    [ObservableProperty] private bool _isRememberMe;
+    [ObservableProperty] private bool _isAvailableOffline;
 
     private readonly IUiAuthenticationService _uiAuthenticationService;
+    private readonly ISettingsService<MelnikovSettings> _settingsService;
     private readonly Func<CancellationToken, ValueTask> _successSignInFunc;
     private readonly Func<CancellationToken, ValueTask> _offlineSignInFunc;
 
-    public SignInViewModel(IUiAuthenticationService uiAuthenticationService, Func<CancellationToken, ValueTask> successSignInFunc, Func<CancellationToken, ValueTask> offlineSignInFunc)
+    public SignInViewModel(IUiAuthenticationService uiAuthenticationService, Func<CancellationToken, ValueTask> successSignInFunc, Func<CancellationToken, ValueTask> offlineSignInFunc, ISettingsService<MelnikovSettings> settingsService)
     {
         _uiAuthenticationService = uiAuthenticationService;
         _successSignInFunc = successSignInFunc;
         _offlineSignInFunc = offlineSignInFunc;
+        _settingsService = settingsService;
+    }
+
+    [RelayCommand]
+    private async Task InitializedAsync(CancellationToken ct)
+    {
+        await WrapCommand(async () =>
+        {
+            var settings = await _settingsService.GetSettingsAsync(ct);
+
+            if (!settings.Token.IsNullOrWhiteSpace())
+            {
+                IsAvailableOffline = true;
+            }
+        });
     }
 
     [RelayCommand]
@@ -35,8 +55,25 @@ public partial class SignInViewModel : ViewModelBase, INonHeader
     {
         await WrapCommand(async () =>
         {
-            if (await UiHelper.CheckValidationErrorsAsync(_uiAuthenticationService.GetAsync(CreateManisGetRequest(), ct)))
+            var response = await _uiAuthenticationService.GetAsync(CreateManisGetRequest(), ct);
+
+            if (await UiHelper.CheckValidationErrorsAsync(response))
             {
+                if (IsRememberMe)
+                {
+                    await _settingsService.SaveSettingsAsync(new()
+                    {
+                        Token = response.SignIns[LoginOrEmail].Token,
+                    }, ct);
+                }
+                else
+                {
+                    await _settingsService.SaveSettingsAsync(new()
+                    {
+                        Token = string.Empty,
+                    }, ct);
+                }
+
                 await _successSignInFunc.Invoke(ct);
             }
         });
